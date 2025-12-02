620336d9f86234abdf66b359f152691e3f3780f0
Monica Coca <mcocapelaz@gmail.com>
2025-12-02 17:59:15 +0100

    state variables and fetch added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,70 @@
 import "../styles/App.scss";
-import {useState, useEffect} from "react"; 
-
+import { useState, useEffect } from "react";
 
 function App() {
+  // Variables de estado
 
+  const [characters, setCharacters] = useState([]);
+  const [searchCharacters, setSearchCharacters] = useState("");
+  const [searchHouse, setSearchHouse] = useState("gryffindor");
+  const [loading, setLoading] = useState(true);
 
-return (
+  // useEffect
+
+  useEffect(() => {
+    fetch("https://hp-api.onrender.com/api/characters/house/gryffindor")
+      .then((response) => response.json())
+      .then((characters) => {
+        setCharacters(characters);
+        setLoading(false);
+      })
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  
+
+  return (
     <div className="container">
       <div className="logo">
-        <img 
-          src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Harry_Potter_wordmark.svg" 
-          alt="Harry Potter" 
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Harry_Potter_wordmark.svg"
+          alt="Harry Potter"
         />
       </div>
-
-      <form className="search-section">
-        <label>Busca por personaje:</label>
-        <input 
-          type="text" 
-          value={searchCharacters}
-          onChange={handleSearch}
-          placeholder="Nombre del personaje"
-        />
-      </form>
 
       <div className="search-section">
-        <label>Selecciona la casa:</label>
-        <select value={"gryffindor"}>
-          <option value="gryffindor">Gryffindor</option>
-          <option value="slytherin">Slytherin</option>
-          <option value="hufflepuff">Hufflepuff</option>
-          <option value="ravenclaw">Ravenclaw</option>
-        </select>
+        <form>
+          <label>Busca por personaje:</label>
+          <input
+            type="text"
+            value={searchCharacters}
+            onChange={handleSearch}
+            placeholder="Nombre del personaje"
+          />
+        </form>
       </div>
 
-      <div className="characters-grid">
-        {/* {characters.map((character) => (
-          <CharacterCard key={character.id} character={character} />
-        ))} */}
+      <div className="search-section">
+        <form>
+          <label>Selecciona la casa:</label>
+          <select value={"gryffindor"} onChange={handleSearch}>
+            <option value="gryffindor">Gryffindor</option>
+            <option value="slytherin">Slytherin</option>
+            <option value="hufflepuff">Hufflepuff</option>
+            <option value="ravenclaw">Ravenclaw</option>
+          </select>
+        </form>
+      </div>
+
+ <div className="characters-grid">
+        <ul>
+          {filteredCharacters.map((character) => (
+            <li key={character.id}>
+              {character.name} {character.house}
+              <img src={character.image} alt={character.name} />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
