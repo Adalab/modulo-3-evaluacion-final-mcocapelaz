ff1b2ba977d36bfc65f104b85ff7ef542cb43199
Monica Coca <mcocapelaz@gmail.com>
2025-12-03 10:52:29 +0100

    houses finder, logos and fill image added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,21 @@
 import "../styles/App.scss";
 import { useState, useEffect } from "react";
-
+import hogwartsCastle from "../images/hogwarts_castle.png";
+import goldenWizard from "../images/golden_wizard.jpg";
 
 function App() {
-  // Variables de estado
-
   const [characters, setCharacters] = useState([]);
   const [searchCharacters, setSearchCharacters] = useState("");
   const [searchHouses, setSearchHouses] = useState("gryffindor");
   const [loading, setLoading] = useState(true);
 
-  // useEffect
-
   useEffect(() => {
-    setLoading(true);  
+    setLoading(true);
     let urlHouses;
     if (searchHouses === "todas") {
-      urlHouses = "https://hp-api.onrender.com/api/characters/"
+      urlHouses = "https://hp-api.onrender.com/api/characters/";
     } else {
-      urlHouses = `https://hp-api.onrender.com/api/characters/house/${searchHouses}`
+      urlHouses = `https://hp-api.onrender.com/api/characters/house/${searchHouses}`;
     }
     fetch(urlHouses)
       .then((response) => response.json())
@@ -28,29 +25,23 @@ function App() {
       })
       .catch((error) => {
         console.error("Error:", error);
-        setLoading(false);  
+        setLoading(false);
       });
   }, [searchHouses]);
 
-  //Variables derivadas
-
   const filteredCharacters = characters.filter((character) =>
     character.name
-      .toLocaleLowerCase()
-      .includes(searchCharacters.toLocaleLowerCase())
+      .toLowerCase()
+      .includes(searchCharacters.toLowerCase())
   );
 
-  // Funciones manejadoras
-
   const handleSearch = (ev) => {
-    setSearchCharacters(ev.target.value) 
+    setSearchCharacters(ev.target.value);
   };
 
   const handleSearchHouses = (ev) => {
-    setSearchHouses(ev.target.value) 
+    setSearchHouses(ev.target.value);
   };
-
-  // Renderizado condicional
 
   if (loading) {
     return <p>Cargando personajes...</p>;
@@ -66,11 +57,16 @@ function App() {
 
   return (
     <div className="container">
+      <div className="logos-container">
+      <div className="logo">
+        <img src={hogwartsCastle} alt="Hogwarts Castle" />
+      </div>
       <div className="logo">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Harry_Potter_wordmark.svg"
           alt="Harry Potter"
         />
+      </div>
       </div>
 
       <div className="search-section">
@@ -100,11 +96,14 @@ function App() {
 
       <div className="characters-grid">
         {filteredCharacters.map((character) => (
-          <div className="character-card" key={character.id}> 
-            <img src={character.image} alt={character.name} />
+          <div className="character-card" key={character.id}>
+            <img
+              src={character.image || goldenWizard}
+              alt={character.name}
+            />
             <div className="character-info">
               <h3>{character.name}</h3>
-              <p>{character.house}</p>
+              <p>{character.species}</p>
             </div>
           </div>
         ))}
@@ -114,5 +113,3 @@ function App() {
 }
 
 export default App;
-
-
